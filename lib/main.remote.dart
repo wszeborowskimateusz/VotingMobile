de9cb085ca9400590ba9210be260c15ac122af5f
Mateusz <wszeborowskimateusz@gmail.com>
2020-11-17 22:22:32 +0100

    Adding remote ips

---
+++ lib/main.remote.dart
@@ -0,0 +1,6 @@
+import 'package:votingmobile/common/config/config.dart';
+import 'package:votingmobile/common/start_app.dart';
+
+void main() async {
+  startApp(Config(apiUrl: "http://34.89.113.193:8080"));
+}
