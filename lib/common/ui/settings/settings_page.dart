33aaa6fe811d842cb0bdd8093ac892939294dff4
Mateusz <wszeborowskimateusz@gmail.com>
2020-12-06 14:08:01 +0100

    Adding easter egg

--- lib/common/ui/settings/settings_page.dart
+++ lib/common/ui/settings/settings_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 import 'package:votingmobile/common/backend/locale_repository.dart';
 import 'package:votingmobile/common/config/config.dart';
 import 'package:votingmobile/common/loader/screen_loader.dart';
@@ -24,6 +28,40 @@ class _SettingPageState extends State<SettingPage> with ScreenLoader {
   };
   final userRepository = locator.get<UserRepository>();
   bool initialized = false;
+  ShakeDetector _shakeDetector;
+  bool _displayEasterEgg = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
+      _listenForShake();
+    }
+  }
+
+  void _listenForShake() {
+    _shakeDetector = ShakeDetector.autoStart(onPhoneShake: () {
+      if (!_displayEasterEgg && mounted) {
+        setState(() {
+          _displayEasterEgg = true;
+        });
+
+        Future.delayed(Duration(seconds: 3), () {
+          if (mounted) {
+            setState(() {
+              _displayEasterEgg = false;
+            });
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _shakeDetector?.stopListening();
+    super.dispose();
+  }
 
   @override
   Widget screen(BuildContext context) {
@@ -32,12 +70,20 @@ class _SettingPageState extends State<SettingPage> with ScreenLoader {
       title: translations.settings,
       child: Container(
         constraints: BoxConstraints(maxWidth: Config.maxElementInAppWidth),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Divider(),
-            _buildLocaleSettings(context),
-            Divider(),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Divider(),
+                _buildLocaleSettings(context),
+                Divider(),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: _buildEasterEgg(),
+            ),
           ],
         ),
       ),
@@ -50,6 +96,20 @@ class _SettingPageState extends State<SettingPage> with ScreenLoader {
               },
             )
           : null,
+    );
+  }
+
+  Widget _buildEasterEgg() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: AnimatedOpacity(
+        opacity: _displayEasterEgg ? 1.0 : 0.0,
+        duration: Duration(milliseconds: 500),
+        child: CircleAvatar(
+          radius: 50,
+          backgroundImage: AssetImage("assets/images/easter_egg.jpg"),
+        ),
+      ),
     );
   }
 
