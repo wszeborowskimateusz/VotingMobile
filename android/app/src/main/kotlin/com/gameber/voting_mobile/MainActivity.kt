40a83ddb681f864330f8320d90462fcb992e6c90
mateusz.wszeborowski <mateusz.wszeborowski@netguru.com>
2022-03-05 11:20:29 +0100

    Updating to new flutter version

---
+++ android/app/src/main/kotlin/com/gameber/voting_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gameber.voting_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/common/settings/theme_change_notifier.dart
+++ lib/common/settings/theme_change_notifier.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:votingmobile/common/backend/theme_repository.dart';
 import 'package:votingmobile/common/locator/locator.dart';
 
