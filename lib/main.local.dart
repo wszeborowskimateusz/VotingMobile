ebd2ea0bff19ef660dc3917a874adc22772fdf54
mateusz.wszeborowski <mateusz.wszeborowski@netguru.com>
2022-03-05 15:15:03 +0100

    Fixing dockerfiles

--- lib/main.local.dart
+++ lib/main.local.dart
@@ -2,5 +2,5 @@ import 'package:votingmobile/common/config/config.dart';
 import 'package:votingmobile/common/start_app.dart';
 
 void main() async {
-  startApp(Config(apiUrl: "http://192.168.99.100:8082"));
+  startApp(Config(apiUrl: "http://localhost:8082"));
 }
